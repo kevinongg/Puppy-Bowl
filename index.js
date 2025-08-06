c0bb7ec20ed14d5f82fe900c0d91d61b2f6c7689
Kevin Ong <kevinongg@gmail.comm>
2025-08-05 20:46:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// === Constants ===
+const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
+const COHORT = "/2507-Kevin"; // Make sure to change this!
+const RESOURCE = "/players";
+const API = BASE + COHORT + RESOURCE;
+console.log(API);
+
+// === State ===
+let puppies = []
+let selectedPuppy =
+
