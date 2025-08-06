6460597f9a387eeff17203acf704e20bdb13e5a1
Kevin Ong <kevinongg@gmail.comm>
2025-08-05 21:11:27 -0400

    added outline of how the app will be

--- index.js
+++ index.js
@@ -6,6 +6,64 @@ const API = BASE + COHORT + RESOURCE;
 console.log(API);
 
 // === State ===
-let puppies = []
-let selectedPuppy =
+let puppies = [];
+// let selectedPuppy =
 
+// === Utility Functions ===
+/** Updates state with all puppies from the API */
+const getPuppies = async () => {};
+
+/** Updates state with a single puppy from the API */
+const getSinglePuppy = async (id) => {};
+
+//////////////// DO LATER AFTER YOU GET THE APP RUNNING ////////////////
+/** USE POST METHOD TO ADD NEW PARTY FROM THE API */
+/** USE DELETE METHOD TO REMOVE THE PARTY FROM THE API WITH THE ID */
+
+// === Components ===
+/** Puppy name that shows more details about the artist when clicked */
+const displayPuppyBoxes = (party) => {};
+/** Add event Listener 
+
+
+/** A list of names of all puppies with pictures */
+const displayPuppyies = () => {};
+
+/** Detailed information about the selected puppy */
+const displayPuppyDetails = () => {};
+//////////////// DO LATER AFTER YOU GET THE APP RUNNING ////////////////
+/** Add event listener delete a puppy */
+
+const addNewPuppyForm = () => {};
+//////////////// DO LATER AFTER YOU GET THE APP RUNNING ////////////////
+/** Add event listener add a puppy to the form */
+
+// === Render ===
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+    <h1>PUPPY BOWL</h1>
+    <main>
+      <section>
+        <h2>Puppy Lineup</h2>
+        <displayPuppies></displayPuppies>
+        <h3>Add a puppy</h3>
+        <addNewPuppyForm></addNewPuppyForm>
+      </section>
+      <section id="selected">
+        <h2>Puppy Details</h2>
+        <displayPuppyDetails></displayPuppyDetails>
+      </section>
+    </main>
+  `;
+  // $app.querySelector("displayPuppies").replaceWith(displayPuppies());
+  // $app.querySelector("addNewPuppyForm").replaceWith(addNewPuppyForm());
+  // $app.querySelector("displayPuppyDetails").replaceWith(displayPuppyDetails());
+}
+
+const init = async () => {
+  // await getParties();
+  render();
+};
+
+init();
